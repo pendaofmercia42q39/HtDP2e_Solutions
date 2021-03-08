e4f967a5626bfe9282efa29b1ae7505b85a96a8e
pendaofmercia42q39 <76793506+pendaofmercia42q39@users.noreply.github.com>
2021-03-08 10:59:41 -0600

    Update ex-20.rkt

--- HtDP2e/I Fixed-Size Data/02 Functions and Programs/ex-20.rkt
+++ HtDP2e/I Fixed-Size Data/02 Functions and Programs/ex-20.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex-20) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "convert.rkt" "teachpack" "htdp")) #f)))
 (define str "hello_world")
 (define ind "01234567890")
 
