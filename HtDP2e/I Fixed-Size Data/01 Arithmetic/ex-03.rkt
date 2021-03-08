6335ce45c8465b3f88498e22457b558d050e2ae2
pendaofmercia42q39 <76793506+pendaofmercia42q39@users.noreply.github.com>
2021-03-08 10:46:11 -0600

    Update ex-03.rkt

--- HtDP2e/I Fixed-Size Data/01 Arithmetic/ex-03.rkt
+++ HtDP2e/I Fixed-Size Data/01 Arithmetic/ex-03.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex-03) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "convert.rkt" "teachpack" "htdp")) #f)))
 (define str "helloworld")
 (define ind "0123456789")
 (define i 5)
