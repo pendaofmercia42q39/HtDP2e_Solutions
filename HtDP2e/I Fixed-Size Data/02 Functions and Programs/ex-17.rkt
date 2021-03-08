363f17d773d8c6d96b3851d7436339b8573e3c3c
pendaofmercia42q39 <76793506+pendaofmercia42q39@users.noreply.github.com>
2021-03-08 11:02:00 -0600

    Update ex-17.rkt

--- HtDP2e/I Fixed-Size Data/02 Functions and Programs/ex-17.rkt
+++ HtDP2e/I Fixed-Size Data/02 Functions and Programs/ex-17.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex-17) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "convert.rkt" "teachpack" "htdp")) #f)))
 (require 2htdp/image)
 
 (define (image-classify img)
