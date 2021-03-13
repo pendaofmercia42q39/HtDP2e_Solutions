9d8f2ed5ab449225b8d56ff690ee05738f55cc0a
pendaofmercia42q39 <76793506+pendaofmercia42q39@users.noreply.github.com>
2021-03-13 11:17:29 -0600

    Update ex-11.rkt

--- HtDP2e/I Fixed-Size Data/02 Functions and Programs/ex-11.rkt
+++ HtDP2e/I Fixed-Size Data/02 Functions and Programs/ex-11.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex-11) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "convert.rkt" "teachpack" "htdp")) #f)))
 ; Turning the expression of ex-01
 ; into the function distance-to-origin
 (define (distance-to-origin x y)
